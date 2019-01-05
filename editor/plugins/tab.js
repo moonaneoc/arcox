0e6f24524f4772de66a4d6a266e972393509e6c9
zsj <zhangshiqq@126.com>
2019-01-06 01:34:29 +0800

    fix tab

--- editor/plugins/tab.js
+++ editor/plugins/tab.js
@@ -6,7 +6,6 @@ module.exports = function (editor) {
         if (e.code === 9) {
             e.preventDefault();
             e.stopPropagation();
-            // editor.setContent(editor.el.value + "\t");
             let { startIndex, endIndex } = editor.getSelection();
             let content = editor.getContent();
             editor.setContent(`${content.substring(0, startIndex)}\t${content.substring(endIndex, content.length)}`);
